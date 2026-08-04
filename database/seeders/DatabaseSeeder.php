0844aec9ebf7ee8f2067b4968e7777b965e7e28a
ShiroNekoe <davenalk612@gmail.com>
2026-08-04 13:55:42 +0700

    feat: add DatabaseSeeder to automate the import of processing reports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,13 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $importer = new ProcessingReportImporter();
-        $importer->import(true);
+        try {
+            $importer = new ProcessingReportImporter();
+            $result = $importer->import(true); // reset = true
+            \Log::info('Seeder completed', $result);
+        } catch (\Exception $e) {
+            \Log::error('Seeder error: ' . $e->getMessage());
+            // Don't throw, just log
+        }
     }
 }
